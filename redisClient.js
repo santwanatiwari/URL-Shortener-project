79355d0c07f93e7bafc38b70242741b1a67711cb
Leetcode Questions <santwanatiwari96@gmail.com>
2026-04-29 23:19:18 +0530

    final redis fix

--- redisClient.js
+++ redisClient.js
@@ -1,12 +1,14 @@
 const { createClient } = require("redis");
 
-const redisClient = createClient();
+const redisClient = createClient({
+  url: process.env.REDIS_URL, 
+});
 
 redisClient.on("error", (err) => console.log("Redis Error", err));
 
 async function connectRedis() {
-    await redisClient.connect();
-    console.log("Redis connected");
+  await redisClient.connect();
+  console.log("Redis connected");
 }
 
 module.exports = { redisClient, connectRedis };
